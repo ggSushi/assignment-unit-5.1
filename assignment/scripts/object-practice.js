da6627148098ecab800d2d4f9ec63a36268d9b27
Glonel Dimapilis <gdimapilis@aol.com>
2023-01-31 20:52:12 -0600

    completed questions in js

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,6 +10,11 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'Glonel',
+  lastName: 'Dimapilis',
+  hasSiblings: true,
+  shoeCount: 6,
+  favThreeFoods: ['baked ziti', 'steak', 'rice'],
 };
 console.log('A little about me:', me);
 
@@ -19,7 +24,11 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-
+//* backticks `` need to be used when using 
+//* template literals when concatenating two variables.
+//* It's cleaner, in my opinion, than using var + ' ' + var
+let fullName = `${me.firstName} ${me.lastName}`;
+console.log(`Hi, my name is`, fullName);
 
 
 /* 3. Nested arrays
@@ -27,7 +36,11 @@ console.log('A little about me:', me);
   - Console.log your last favorite food 
 */
 
-
+console.log(`first fave food is`, me.favThreeFoods[0]);
+//? I've found that in order to get the .length to work, 
+//? I need to make sure I mention the object it's in 
+//? as well as the arrayName within it.
+console.log(`Last fave food is`, me.favThreeFoods[me.favThreeFoods.length - 1]);
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
@@ -36,6 +49,9 @@ console.log('A little about me:', me);
   - Console.log your updated shoe count. 
 */
 
+console.log(`I have this many shoes:`, me.shoeCount);
+me.shoeCount += 1;
+console.log(`Got new shoes! Now I have ${me.shoeCount} shoes!` );
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +59,8 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'purple';
+console.log(`About me:`, me); 
+
+//! End assignment
